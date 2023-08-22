9e6668b94f1d5eec54b90a19e9ee5f448f32476c
Rixiov10 <rixiov10@gmail.com>
2023-08-21 22:38:01 -0400

    cambios de ultima hora

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './componentes/navbar/NavBar';
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './componentes/ItemDetailContainer/ItemDetailContainer';
+import { CartProvider } from './Context/CartContext';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path='/' element ={<ItemListContainer/>}/>
-          <Route path='/categoria/:categoryId' element={<ItemListContainer />} />
-          <Route path='/item/:itemId' element={<ItemDetailContainer />} />
-          <Route path='*' element={<h1>404 NOT FOUND</h1>} />
-        </Routes>
+        <CartProvider>
+          <NavBar />
+          <Routes>
+            <Route path='/' element ={<ItemListContainer/>}/>
+            <Route path='/categoria/:categoryId' element={<ItemListContainer />} />
+            <Route path='/item/:itemId' element={<ItemDetailContainer />} />
+            <Route path='/cart' element={<cart/>} />
+            <Route path='*' element={<h1>404 NOT FOUND</h1>} />
+          </Routes>
+        </CartProvider>
       </BrowserRouter>
     </div>
   );
--- src/componentes/ItemDetail/ItemDetail.js
+++ src/componentes/ItemDetail/ItemDetail.js
@@ -1,8 +1,25 @@
 import "./ItemDetail.css"
+import { useContext, useState } from "react"
 import ItemCount from "../ItemCount/ItemCount"
+import { CartContext } from "../../Context/CartContext"
+import { Link } from "react-router-dom"
 
 
 const ItemDetail = ({id, nombre, categoria, precio, imagen}) => {
+    const [quantiyAdded, setQuantityAdded]= useState(0)
+
+    const {addItem} = useContext(CartContext)
+
+    const handledOnAdd = (quantity) => {
+        setQuantityAdded(quantity)
+
+        const item = {
+            id, nombre, precio
+        }
+
+        addItem(item, quantity)
+    }
+
     return (
         <article className= "CardItem">
             <header className= "Header">
@@ -22,7 +39,13 @@ const ItemDetail = ({id, nombre, categoria, precio, imagen}) => {
                 </p>
             </section>
             <footer className='itemFooter'>
-                <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('cantidad agregada ', quantity)}/>
+                {
+                    quantiyAdded > 0 ? (
+                        <Link to='/cart' className='option'>Terminar compra</Link>
+                    ) : (
+                        <ItemCount initial={1} stock={10} onAdd={(handledOnAdd)}/>
+                        )
+                }
             </footer>
         </article>
     )
